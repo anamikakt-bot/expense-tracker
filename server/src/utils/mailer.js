61302e649acafed6c6297fa6e5c91a0b5fc885c7
Anamika <anamikakt@shnoor.com>
2026-09-09 15:19:28 +0530

    fix: switiched from Nodemailer/Gmail SMTP to Resend HTTP to work with Render's SMTP port blocking

--- server/src/utils/mailer.js
+++ server/src/utils/mailer.js
@@ -1,16 +1,10 @@
-const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 exports.sendResetEmail = async (to, resetLink) => {
-  await transporter.sendMail({
-    from: `"Expense Tracker" <${process.env.EMAIL_USER}>`,
+  await resend.emails.send({
+    from: 'Expense Tracker <onboarding@resend.dev>',
     to,
     subject: 'Reset your password',
     html: `
